0f522134219d51a32a57a80a0907ee3795f1addd
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 15:09:19 +0430

    store

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import Home from "./HomePage";
+import Products from "./ProductsPage";
+import singleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
 
-export{
-    Home
-}
+export {
+  Home,
+  Products,
+  singleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+};

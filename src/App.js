8e17405f897f63675057cd77afef5cad6bd7ca70
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 14:10:18 +0430

    store

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
-import styled from "styled-components";
-
-
-
-const Div = styled.div`
-background:blue;
-color:black;
-`;
+import { Home } from "./pages";
 
 function App() {
   return (
     <div>
       <h4>comfy sloth starter</h4>
+      <Home/>
     </div>
   );
 }

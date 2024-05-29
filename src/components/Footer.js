29f85b9caed61cda4becff2d5fe96c8dd26ad5fc
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 17:09:34 +0430

    store

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <Wrapper>
-      <h5>&copy; {new Date().getFullYear()}
-      <span>comfySloth</span>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span>comfySloth</span>
       </h5>
       <h5>All rights reserved</h5>
     </Wrapper>

5361c8ec902febfc07c9e1620a65cc3879429bdf
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 19:58:50 +0430

    store

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -26,9 +26,13 @@ export const ProductsProvider = ({ children }) => {
   const openSidebar = () => {
     dispatch({ type: "SIDEBAR_OPEN" });
   };
+  const closeSidebar = () => {
+    dispatch({ type: "SIDEBAR_OPEN" });
+  };
 
   useEffect(() => {
     openSidebar();
+    closeSidebar
   }, []);
 
   return (

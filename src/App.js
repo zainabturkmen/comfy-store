ddb0e17831311a9c92afd9a4d3b9e4972684f39c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 10:45:04 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+import styled from "styled-components"
+
+function App() {
+  return <h4>comfy sloth starter</h4>
+}
+
+export default App

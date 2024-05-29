7b68b7d981333097fa4e0293bda59d3d2d7cd890
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-29 15:53:08 +0430

    store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
-import CartButtons from './CartButtons'
-import { useProductsContext } from '../context/products_context'
-import { useUserContext } from '../context/user_context'
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { links } from "../utils/constants";
+import CartButtons from "./CartButtons";
+import { useProductsContext } from "../context/products_context";
+import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <NavContainer>
+      <div className="nav-center">
+          <div className="nav-header">
+            
+          </div>
+      </div>
+    </NavContainer>
+  );
+};
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -77,6 +85,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`
+`;
 
-export default Nav
+export default Nav;

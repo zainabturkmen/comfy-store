f594e8981ed382972d0761b8b37cb6210009fa47
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 09:34:22 +0430

    store

--- src/components/Services.js
+++ src/components/Services.js
@@ -19,7 +19,12 @@ const Services = () => {
         </article>
         <div className="services-center">
           {services.map((service) => {
-            const { id, icone, title, text } = service;
+            const { id, icon, title, text } = service;
+            return <article key={id} className="service">
+              <span className="icon">{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
           })}
         </div>
       </div>

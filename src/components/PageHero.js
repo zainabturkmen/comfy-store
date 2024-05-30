cb43b9dc6fcde5d32de9a5bdbb23c5cfa76ca390
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 08:32:00 +0430

    store

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -5,7 +5,7 @@ const PageHero = ({title}) => {
   return <Wrapper>
     <div className='section-center'>
       <h3>
-        <Link to="/">Home</Link>
+        <Link to="/">Home</Link> / {title}
       </h3>
     </div>
   </Wrapper>

4334b44bc8cb4656fec15c09e2469525bafb2f3f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 14:53:22 +0430

    store

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -32,7 +32,7 @@ const products_reducer = (state, action) => {
     };
   }
   if (action.type === GET_PRODUCTS_ERROR) {
-    return { ...state, products_loading: false, products_error: true };
+    return { ...state, products_loading: false, products_error: false };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);

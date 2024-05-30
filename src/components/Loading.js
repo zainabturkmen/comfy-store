04223140f9b88cc56a98b8b349f3b4aff5046ade
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 14:18:45 +0430

    store

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
-  return <div className='section section-center'></div>
-}
+  return (
+    <div className="section section-center">
+      <div className="loading"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;

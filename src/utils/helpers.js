e9ddc21b67d818ccd4ae5195a620b9affe6b370a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 16:12:14 +0430

    store

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,8 @@ export const formatPrice = (number) => {
   const newNumber = Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  }).format(number);
-  return;
+  }).format(number/100);
+  return newNumber; 
 };
 
 export const getUniqueValues = () => {};

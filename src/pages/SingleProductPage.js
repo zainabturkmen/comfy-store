098750090dc25ec621b49aeffd3c363e3c3b403d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 19:55:11 +0430

    store

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -16,6 +16,7 @@ import { Link } from "react-router-dom";
 
 const SingleProductPage = () => {
   const { id } = useParams();
+  const history = useHistory()
   const {
     single_product_loading: loading,
     single_product_error: error,
@@ -26,6 +27,16 @@ const SingleProductPage = () => {
   useEffect(() => {
     fetchSingleProducts(`${url}${id}`);
   }, [id]);
+
+  useEffect(() => {
+    console.log(error);
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+  },[error])
+
 
   if (loading) {
     return <Loading />

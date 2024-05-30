48414d07085061ce52aebe9e38d73487bb372e92
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 15:55:25 +0430

    store

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -40,7 +40,7 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_PRODUCTS_BEGIN });
     try {
       const response = await axios.get(url);
-      const products = await response.data();
+      const products = await response.data;
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
     } catch (error) {
       console.log(error);

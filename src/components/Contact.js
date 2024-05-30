6065b5ffdfa2555182756b5bcb4a79885170e23d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-30 09:36:50 +0430

    store

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <Wrapper>
-    <div className='section-center'></div>
-  </Wrapper>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+            labore dolorem ea praesentium, laboriosam tempore ut doloremque
+            voluptas quia voluptatibus.
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -69,6 +80,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;

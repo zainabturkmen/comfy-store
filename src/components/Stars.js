f8058c47ba2557b1039f728280643557efe58654
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-31 12:01:07 +0430

    store

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,61 +3,16 @@ import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 const Stars = ({ stars, reviews }) => {
-
-<span>
+  {
+    /* <span>
   {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
-</span>;
+</span>; */
+  }
 
   return (
     <Wrapper>
       <div className="stars">
-        {/* star */}
         
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
       </div>
       <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>

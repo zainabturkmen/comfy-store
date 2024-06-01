fc4abd243cfc71274123232fc6b91fc3c75fd872
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 18:42:45 +0430

    store

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -9,9 +9,9 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
-import filter_reducer from '../reducers/filter_reducer'
+} from "../actions";
+import { useProductsContext } from "./products_context";
+import filter_reducer from "../reducers/filter_reducer";
 
 const initialState = {
   filtered_products: [],
@@ -19,31 +19,35 @@ const initialState = {
   grid_view: true,
 };
 
-const FilterContext = React.createContext()
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
-  const {products} = useProductsContext();
-  const [state, dispatch] = useReducer(reducer, initialState );
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    dispatch({type: LOAD_PRODUCTS, payload:products})
-  }, [products])
-
-
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
 
   const setGridView = () => {
-    
-  }
+    dispatch({ type: SET_GRIDVIEW });
+  };
+  
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
 
   return (
-    <FilterContext.Provider value= {{
-      ...state,
-    }}>
+    <FilterContext.Provider
+      value={{
+        ...state,
+      }}
+    >
       {children}
     </FilterContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};

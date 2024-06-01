b2b548588f7eee004c0107ff8ebe262f47d881a5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 12:48:36 +0430

    store

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -8,6 +8,10 @@ const AmountButtons = ({ increase, decrease, amount }) => {
       <button type="button" className="amount-btn" onClick={decrease}>
         <FaMinus />
       </button>
+      <h2 className="amount">{amount}</h2>
+      <button type="button" className="amount-btn" onClick={increase}>
+        <FaPlus />
+      </button>
     </Wrapper>
   );
 };

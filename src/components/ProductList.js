fd2d81cace479c9b8fc0afa0404c6b507823b9c3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 16:13:37 +0430

    store

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,7 @@ import ListView from "./ListView";
 
 const ProductList = () => {
   const { filtered_products: products } = useFilterContext();
-  return <GridView></GridView>;
+  return <GridView products={products}>product list</GridView>;
 };
 
 export default ProductList;

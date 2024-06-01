1b4820c8d8a1cecec3e223c037ff3e0b0550ac95
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 09:41:16 +0430

    store

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -83,7 +83,7 @@ const SingleProductPage = () => {
             {company}
           </p>
           <hr/>
-          {stock > 0 && <AddToCart />}
+          {stock > 0 && <AddToCart  product={product}/>}
         </section>
       </div>
     </div>

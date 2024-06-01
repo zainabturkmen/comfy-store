06bfc9883877f0f823a222214d9a9d54f1c22346
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 19:05:40 +0430

    store

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -21,6 +21,9 @@ const filter_reducer = (state, action) => {
   if ( action.type === SET_GRIDVIEW) {
     return { ...state, grid_view: true};
   }
+  if ( action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false};
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 

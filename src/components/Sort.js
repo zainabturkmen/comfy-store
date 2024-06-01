7eba6a48feff50f1eed21bc8dd7483ebcdec7819
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-01 17:55:57 +0430

    store

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
+import Product from './Product'
 
 const Sort = () => {
   const { filtered_products: products, grid_view } = useFilterContext();
@@ -15,6 +16,19 @@ const Sort = () => {
           <BsList />
         </button>
       </div>
+      <p>
+        {products.length} products found
+      </p>
+      <hr/>
+      <form>
+        <label htmlFor='sort'>sort by</label>
+        <select name='sort' id='sort' className='sort-input'>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-lowest">price (lowest)</option>
+        </select>
+      </form>
     </Wrapper>
   );
 }

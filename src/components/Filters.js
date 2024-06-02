e9ac211e451d9c12306c75a361e2356f4f679f5c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 15:52:45 +0430

    store

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -66,7 +66,15 @@ const Filters = () => {
           {/*end of categories */}
           {/* companies */}
           <div className="form-control">
-            
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies}
+            </select>
           </div>
           {/* end of companies */}
         </form>

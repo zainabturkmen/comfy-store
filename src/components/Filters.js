e7dfc86d50b7ee4c55d79188431bc266558e2d08
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 13:51:11 +0430

    store

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFilterContext } from '../context/filter_context'
-import { getUniqueValues, formatPrice } from '../utils/helpers'
-import { FaCheck } from 'react-icons/fa'
+import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { getUniqueValues, formatPrice } from "../utils/helpers";
+import { FaCheck } from "react-icons/fa";
 
 const Filters = () => {
-  return <h4>filters</h4>
-}
+  const {
+    filters: {
+      text,
+      category,
+      company,
+      color,
+      main_price,
+      price,
+      max_price,
+      shipping,
+    },
+  } = useFilterContext();
+  return <Wrapper>filters</Wrapper>;
+};
 
 const Wrapper = styled.section`
   .form-control {
@@ -106,6 +118,6 @@ const Wrapper = styled.section`
       top: 1rem;
     }
   }
-`
+`;
 
-export default Filters
+export default Filters;

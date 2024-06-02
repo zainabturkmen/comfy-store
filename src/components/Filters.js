c71a0a188258da526d38a1011162700c0ef280cb
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 17:32:23 +0430

    store

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -135,9 +135,9 @@ const Filters = () => {
           </div>
           {/* end of price */}
           {/* shipping */}
-          <div className="form-control">
+          <div className="form-control shipping">
             <label htmlFor="shipping">free shipping</label>
-            <input type="checkbox" name="shipping" id="shipping" />
+            <input type="checkbox" name="shipping" id="shipping" onChange={updateFilters} checked={shipping}/>
           </div>
           {/* end of shipping */}
         </form>

9565c233e433cd6962e7665ca665f109749143eb
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 15:03:48 +0430

    store

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -23,8 +23,8 @@ const Filters = () => {
 
 
   const categories = getUniqueValues(all_products, "category");
-  const companies = getUniqueValues(all_products, "category");
-  const categories = getUniqueValues(all_products, "category");
+  const companies = getUniqueValues(all_products, "company");
+  const colors = getUniqueValues(all_products, "colors");
 
   return (
     <Wrapper>

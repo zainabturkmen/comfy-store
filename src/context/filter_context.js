666474be86eca1321abf760c7079aa9dbd0aa6f0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 14:02:30 +0430

    store

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -63,8 +63,7 @@ export const FilterProvider = ({ children }) => {
   const updateFilters = (e) => {
     let name  = e.target.name
     let value = e.target.value
-    console.log(name, value );
-
+    dispatch({type:UPDATE_FILTERS, payload:{name, value}})
   };
 
   const clearFilters = () => {};

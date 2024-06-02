0c4f3eeafd9e3dd89e02a188a5337f989be3da89
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-02 20:22:37 +0430

    store

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -58,6 +58,7 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
   if (action.type === FILTER_PRODUCTS) {
+    
     return { ...state };
   }
   if (action.type === CLEAR_FILTERS) {

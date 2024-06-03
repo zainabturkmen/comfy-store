1b91d59704296dc0d3f2b0193b40727bbef5c459
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-03 15:11:08 +0430

    store

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -16,7 +16,16 @@ const CartContent = () => {
       })}
       <hr />
       <div className="link-container">
-        <Link to="/products" className="link-btn">continue shopping</Link>
+        <Link to="/products" className="link-btn">
+          continue shopping
+        </Link>
+        <button
+          type="button"
+          className="link-btn clear-btn"
+          onClick={clearCart}
+        >
+          clear shopping cart
+        </button>
       </div>
     </Wrapper>
   );

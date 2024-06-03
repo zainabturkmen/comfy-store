f0f4fcdf8ff2551208679207bec38d5de5a66f24
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-03 08:55:19 +0430

    store

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -73,11 +73,18 @@ const filter_reducer = (state, action) => {
       });
     }
 
-    // category 
+    // category
     if (category !== "all") {
-      tempProducts  = tempProducts.filter((product)=> product.category === category)
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
     }
     // company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
 
     return { ...state, filtered_products: tempProducts };
   }

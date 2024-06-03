dedcace11230354e1a49fa85fe1bcf2663b6c223
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-03 14:23:10 +0430

    store

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/cart_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
 import {
   ADD_TO_CART,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
   COUNT_CART_TOTALS,
-} from '../actions'
-import { products_url } from '../utils/constants';
+} from "../actions";
+import { products_url } from "../utils/constants";
 
 const initialState = {
   cart: [],
@@ -16,37 +16,37 @@ const initialState = {
   shipping_fee: 534,
 };
 
-const CartContext = React.createContext()
+const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-
   // add to cart
   const AddToCart = (id, color, amount, product) => {
-    dispatch({type: ADD_TO_CART, payload:{id, color, amount, product}})
-  }
-
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
 
   // remove item
-  const removeItem = (id) => {}
+  const removeItem = (id) => {};
   // toggle amount
-  const toggleAmount = (id, value) => {}
+  const toggleAmount = (id, value) => {};
 
   // clear cart
-  const clearCart = () => {}
-
-
+  const clearCart = () => {};
 
   useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(state.cart))
-  },[state.cart])
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
-    <CartContext.Provider value={{...state, AddToCart, removeItem, toggleAmount, clearCart}}>{children}</CartContext.Provider>
-  )
-}
+    <CartContext.Provider
+      value={{ ...state, AddToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 // make sure use
 export const useCartContext = () => {
-  return useContext(CartContext)
-}
+  return useContext(CartContext);
+};

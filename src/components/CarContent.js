ff84e3aa58c4a095634c8d06ed15e4629189c4a7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-03 15:01:45 +0430

    store

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -7,6 +7,7 @@ import CartItem from './CartItem'
 import CartTotals from './CartTotals'
 
 const CartContent = () => {
+  const {cart, clearCart} = useCartContext()
   return <h4>cart content </h4>
 }
 const Wrapper = styled.section`

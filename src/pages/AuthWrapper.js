0312795c4293ed064251d9347c4376ff5b5938ef
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-04 17:45:10 +0430

    store

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import styled from 'styled-components'
 
-const AuthWrapper = () => {
+const AuthWrapper = ({children}) => {
   return <h4>AuthWrapper Component</h4>
 }
 

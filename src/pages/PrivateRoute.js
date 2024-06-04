cf61d63efe43b248589a0e924b98039062a6967d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-04 17:40:30 +0430

    store

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,6 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({children, ...rest}) => {
+  const {user} = useAuth0();
   const {myUser} = useUserContext()
   return <Route {...rest} render={() => {
     return myUser ? children : <Redirect to="/" />

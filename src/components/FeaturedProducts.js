eab9af136f087d75c74b46a9d3b38b29780af245
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-04 18:06:03 +0430

    store

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -30,7 +30,7 @@ const FeaturedProducts = () => {
           return <Product key={product.id} {...product} />;
         })}
       </div>
-      <Link to="/products">all products</Link>
+      <Link to="/products" className="btn">all products</Link>
     </Wrapper>
   );
 };

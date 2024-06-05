7d3b6e87566011a2bdcf4cc16e4c8326a495fa15
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-05 16:16:35 +0430

    adding payment logic

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,9 @@
+// domain/.netlify/functions/create-payment-intent
+
+exports.handler = async function (event, context) {
+  console.log(event);
+  return {
+    statusCode: 200,
+    body: "payment intent",
+  };
+};

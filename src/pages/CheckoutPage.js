261d1ee5ead4d77dc0d66ce2a1fc41d3f23caa94
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-05 09:00:09 +0430

    functions

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,12 +6,23 @@ import { useCartContext } from "../context/cart_context";
 import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
   return (
     <main>
-      <PageHero title="checkout"/>
-        <Wrapper className="page">
-          <h1>checkout here</h1>
-        </Wrapper>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        {cart.length < 1 ? (
+          <div className="empty">
+            <h2>your cart is empty</h2>
+            <Link to="/products" className="btn">
+              fill it
+            </Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
+        <h1>checkout here</h1>
+      </Wrapper>
     </main>
   );
 };

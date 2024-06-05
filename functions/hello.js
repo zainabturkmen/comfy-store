713ccda743e587ba4043f8c3c9536be9406c0f28
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-05 08:18:35 +0430

    functions

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,14 @@
 // domain/.netlify/functions/hello
 
+const items = [
+  { id: 1, name: "zainab" },
+  { id: 2, name: "john" },
+  { id: 3, name: "suzan" },
+];
+
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: "hello world",
+    body: JSON.stringify(items),
   };
 };

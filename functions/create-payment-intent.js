b69e0352d2c717fc29843fd5667aa3b8d7b1dffc
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-06-06 08:58:34 +0430

    updating to react version 6

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -4,32 +4,32 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.REACT_APP_AUTH_STRIPE_SECRET_KEY);
 
 exports.handler = async function (event, context) {
-  if (event.body) {
-    const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
-    console.log(cart);
+  //   if (event.body) {
+  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+  console.log(cart);
 
-    const calculateOrderAmount = () => {
-      return shipping_fee + total_amount;
-    };
-
-    try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(),
-        currency: "usd",
-      });
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ msg: error.message }),
-      };
-    }
-  }
-  return {
-    statusCode: 200,
-    body: "Create Payment Intent",
+  const calculateOrderAmount = () => {
+    return shipping_fee + total_amount;
   };
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(),
+      currency: "usd",
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: error.message }),
+    };
+  }
 };
+return {
+  statusCode: 200,
+  body: "Create Payment Intent",
+};
+// };
